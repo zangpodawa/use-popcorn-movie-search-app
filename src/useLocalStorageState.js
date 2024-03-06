e72d4885669c988d323ddb621b34e0b7e266129f
Dawa Zangpo <32727869+zangpodawa@users.noreply.github.com>
2024-03-06 16:03:06 +1000

    First commit

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  // const [watched, setWatched] = useState([]);
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
